0163ab3c81e0a28c13793ed34f406a9137642206
Mayank Srivastava <mayank21srivastava@gmail.com>
2026-02-17 20:59:31 +0530

    Test Commit

--- sPattern.cpp
+++ sPattern.cpp
@@ -1,27 +1,29 @@
-#include<iostream>
+#include <conio.h>
+#include <iostream>
 
 using namespace std;
 
-int main()  {
-    int n = 7;
-    for ( int i = 0; i < n; i++ ){
-        for ( int j = 0; j < n-1; j++ ){
-            if( i == 0 || i == n-1 || i == n/2) {
-                cout << "*";
-            } else {
-                if ( i + j < n/2 ){
-                    cout << "*";
-                    break;
-                } else if ( i + j > n/2 ){
-                    if( j == n-2 ){
-                        cout << "*";
-                    } else {
-                        cout << " ";
-                    }
-                } 
-            }
+int main() {
+  int n = 7;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - 1; j++) {
+      if (i == 0 || i == n - 1 || i == n / 2) {
+        cout << "*";
+      } else {
+        if (i + j < n / 2) {
+          cout << "*";
+          break;
+        } else if (i + j > n / 2) {
+          if (j == n - 2) {
+            cout << "*";
+          } else {
+            cout << " ";
+          }
         }
-        cout << endl;
+      }
     }
-    return 0;
+    cout << endl;
+  }
+  getch();
+  return 0;
 }
