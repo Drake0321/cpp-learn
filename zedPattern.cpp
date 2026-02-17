84ebadf339f8c7e65b5d689e105cdd174ef95a62
Mayank Srivastava <mayank21srivastava@gmail.com>
2026-02-17 21:05:53 +0530

    Fixing

--- zedPattern.cpp
+++ zedPattern.cpp
@@ -1,23 +1,22 @@
-
+#include <conio.h>
 #include <iostream>
-#include<conio.h>
 using namespace std;
 
 int main() {
- int n = 5;
- for ( int i = 0; i < n; i++ ){
-  for ( int j = 0; j < n; j++ ){
-    if( i == 0 || i == n-1 ){
-      cout<<"*";
-    } else {
-      if( i+j == n -1){
-        cout<<"*";
-      } else if ( i + j < n-1){
-        cout<<" ";
+  int n = 5;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == 0 || i == n - 1) {
+        cout << "*";
+      } else {
+        if (i + j == n - 1) {
+          cout << "*";
+        } else if (i + j < n - 1) {
+          cout << " ";
+        }
       }
     }
+    cout << endl;
   }
-  cout<<endl;
- }
   getch();
 }
